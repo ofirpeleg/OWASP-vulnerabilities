381903d8b982c4d4b916395c08e6b3c03e82d9ad
Ofir Peleg <80089230+ofirpeleg@users.noreply.github.com>
2024-09-22 12:38:17 +0300

    Delete attack.py

--- attack.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:5000/reserve'
-seats = [f"{row}{col}" for row in range(2, 11) for col in range(1, 15)]
-attacker = "attacker"
-
-response = requests.post(url, json={'user': attacker, 'seats': seats})
-print(f'Attempt to reserve seats, Response: {response.json()}')
-
